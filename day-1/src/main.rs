96a3f5fc16790137cf79a93ec1a8b62ce6836471
Jeroen Rooijmans <jhmrooijmans@icloud.com>
2024-12-01 18:06:56 +0100

    Setup and Day 1
    Add input.txt to gitignore

---
+++ day-1/src/main.rs
@@ -0,0 +1,82 @@
+// Advent of Code - Day 1: Historian Hysteria
+
+const INPUT: &str = include_str!("./input.txt");
+
+fn parse_input(input: &str) -> (Vec<u32>, Vec<u32>) {
+    input
+        .lines()
+        .map(|line| {
+            let mut parts = line.split_whitespace();
+            (
+                parts.next().unwrap().parse::<u32>().unwrap(),
+                parts.next().unwrap().parse::<u32>().unwrap(),
+            )
+        })
+        .unzip()
+}
+
+fn solve_part_one(input: &str) -> u32 {
+    let (mut left, mut right) = parse_input(input);
+
+    // sort lists
+    left.sort();
+    right.sort();
+
+    // compute sum of distances
+    let total_distance = left
+        .iter()
+        .zip(right.iter())
+        .map(|(l, r)| l.abs_diff(*r))
+        .sum();
+
+    total_distance
+}
+
+fn solve_part_two(input: &str) -> u32 {
+    let (left, right) = parse_input(input);
+
+    // compute similarity score by multiplies each element in left list with its count in right list
+    let similarity_score = left
+        .iter()
+        .map(|l| {
+            let count = right.iter().filter(|r| *r == l).count() as u32;
+            l * count
+        })
+        .sum::<u32>();
+
+    similarity_score
+}
+
+fn main() {
+    let part_one_answer = solve_part_one(INPUT);
+    println!("Part one:\n{part_one_answer}");
+    let part_two_answer = solve_part_two(INPUT);
+    println!("Part two:\n{part_two_answer}");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part1() {
+        let example_input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        let answer = crate::solve_part_one(example_input);
+        assert_eq!(answer, 11);
+    }
+
+    #[test]
+    fn part2() {
+        let example_input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        let answer = crate::solve_part_two(example_input);
+        assert_eq!(answer, 31);
+    }
+}
