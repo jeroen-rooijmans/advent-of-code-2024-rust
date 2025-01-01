2bdf9267967e515a3e095d8538fe131757149db1
Jeroen Rooijmans <832720+jeroen-rooijmans@users.noreply.github.com>
2025-01-01 14:50:50 +0100

    Day 9

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -3,13 +3,29 @@ use std::cmp::Ordering;
 
 const INPUT: &str = include_str!("./input.txt");
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Block {
     File { id: usize, size: u32 },
-    Empty(u32),
+    Empty { size: u32 },
 }
 
-#[derive(Debug)]
+impl Block {
+    fn id(&self) -> Option<usize> {
+        match self {
+            Block::File { id, .. } => Some(*id),
+            Block::Empty { .. } => None,
+        }
+    }
+
+    fn size(&self) -> u32 {
+        match self {
+            Block::File { size, .. } => *size,
+            Block::Empty { size } => *size,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 struct DiskMap {
     blocks: Vec<Block>,
 }
@@ -25,7 +41,7 @@ impl FromIterator<u32> for DiskMap {
                     blocks.push(Block::File { id, size });
                     id += 1;
                 } else {
-                    blocks.push(Block::Empty(size))
+                    blocks.push(Block::Empty { size })
                 }
             }
         }
@@ -47,29 +63,25 @@ impl DiskMap {
                             (current_pos..new_pos).fold(acc, |sum, pos| sum + pos * (*id as u64));
                         (sum, new_pos)
                     }
-                    Block::Empty(size) => (acc, current_pos + (*size as u64)),
+                    Block::Empty { size } => (acc, current_pos + (*size as u64)),
                 },
             )
             .0
     }
 
-    fn has_gaps(&self) -> bool {
-        self.blocks.iter().any(|b| matches!(b, Block::Empty(_)))
+    fn max_id(&self) -> Option<usize> {
+        self.blocks.iter().filter_map(|b| b.id()).max()
     }
 
-    fn trim_from_right(&mut self) {
-        if let Some(last_file_pos) = self
-            .blocks
+    fn find_idx(&self, target_id: usize) -> Option<usize> {
+        self.blocks
             .iter()
-            .rposition(|block| matches!(block, Block::File { .. }))
-        {
-            self.blocks.truncate(last_file_pos + 1);
-        }
+            .position(|b| matches!(b, Block::File { id, .. } if *id == target_id))
     }
 
     fn reduce_segment(&mut self, idx: usize, amount: u32) {
         match &mut self.blocks[idx] {
-            Block::Empty(size) | Block::File { size, .. } => match &amount.cmp(size) {
+            Block::Empty { size } | Block::File { size, .. } => match &amount.cmp(size) {
                 Ordering::Greater => {
                     panic!("Cannot reduce block {idx} of size {size} by {amount}!")
                 }
@@ -82,18 +94,41 @@ impl DiskMap {
     }
 
     fn compress(&mut self) {
-        while self.has_gaps() {
-            self.trim_from_right();
-            if let Some(empty_idx) = self
-                .blocks
-                .iter()
-                .position(|b| matches!(b, Block::Empty(_)))
-            {
-                if let Block::File { id, .. } = self.blocks[self.blocks.len() - 1] {
-                    self.reduce_segment(self.blocks.len() - 1, 1);
-                    self.reduce_segment(empty_idx, 1);
-                    self.blocks.insert(empty_idx, Block::File { id, size: 1 });
+        let mut block_idx = 0;
+        while block_idx < self.blocks.len() {
+            if let Block::Empty { size } = self.blocks[block_idx] {
+                for _ in 0..size {
+                    self.reduce_segment(block_idx, 1);
+                    if let Some(last_file_idx) = self
+                        .blocks
+                        .iter()
+                        .rposition(|block| matches!(block, Block::File { .. }))
+                    {
+                        if last_file_idx < block_idx {
+                            break;
+                        }
+                        let id = self.blocks[last_file_idx].id().unwrap();
+                        self.reduce_segment(last_file_idx, 1);
+                        self.blocks.insert(block_idx, Block::File { id, size: 1 });
+                        block_idx += 1;
+                    }
                 }
+            }
+            block_idx += 1;
+        }
+    }
+
+    fn compressv2(&mut self) {
+        for file_id in (0..=self.max_id().unwrap()).rev() {
+            let file_idx = self.find_idx(file_id).unwrap();
+            if let Some(empty_idx) = self.blocks[..file_idx].iter().position(|b| {
+                matches!(b, Block::Empty { .. }) && b.size() >= self.blocks[file_idx].size()
+            }) {
+                let file = self.blocks.remove(file_idx);
+                self.blocks
+                    .insert(file_idx, Block::Empty { size: file.size() });
+                self.blocks.insert(empty_idx, file);
+                self.reduce_segment(empty_idx + 1, file.size());
             }
         }
     }
@@ -106,8 +141,11 @@ fn solve_part_one(input: &str) -> u64 {
     disk_map.checksum()
 }
 
-fn solve_part_two(_input: &str) -> usize {
-    42
+fn solve_part_two(input: &str) -> u64 {
+    let input = input.chars().flat_map(|c| c.to_digit(10));
+    let mut disk_map = DiskMap::from_iter(input);
+    disk_map.compressv2();
+    disk_map.checksum()
 }
 
 fn main() {
@@ -121,7 +159,6 @@ fn main() {
 mod tests {
     #[test]
     fn part1() {
-        // let example_input = "12345";
         let example_input = "2333133121414131402";
         let answer = crate::solve_part_one(example_input);
         assert_eq!(answer, 1928);
@@ -131,6 +168,6 @@ mod tests {
     fn part2() {
         let example_input = "2333133121414131402";
         let answer = crate::solve_part_two(example_input);
-        assert_eq!(answer, usize::MAX);
+        assert_eq!(answer, 2858);
     }
 }
