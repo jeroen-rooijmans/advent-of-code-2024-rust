6f8fd8be4053808a4ce0a9ff0a7b0a4de1a1ac92
Jeroen Rooijmans <832720+jeroen-rooijmans@users.noreply.github.com>
2025-07-17 18:07:20 +0200

    Coordinate new implementation and added tests

--- aoc/src/coord.rs
+++ aoc/src/coord.rs
@@ -52,6 +52,12 @@ impl<T: SubAssign> SubAssign for Coordinate<T> {
     }
 }
 
+impl<T> Coordinate<T> {
+    pub fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
 impl Coordinate<usize> {
     pub fn adjacent(self) -> [Option<Self>; 4] {
         let up = (self.y > 0).then(|| self - Self { x: 0, y: 1 });
@@ -88,11 +94,73 @@ impl Coordinate<usize> {
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
 
-    // #[test]
-    // fn it_works() {
-    //     let result = add(2, 2);
-    //     assert_eq!(result, 4);
-    // }
+    #[test]
+    fn test_new() {
+        let coord = Coordinate::new(3, 5);
+        assert_eq!(coord.x, 3);
+        assert_eq!(coord.y, 5);
+    }
+
+    #[test]
+    fn test_add() {
+        let a = Coordinate::new(2, 3);
+        let b = Coordinate::new(5, 7);
+        assert_eq!(a + b, Coordinate::new(7, 10));
+    }
+
+    #[test]
+    fn test_sub() {
+        let a = Coordinate::new(8, 6);
+        let b = Coordinate::new(2, 4);
+        assert_eq!(a - b, Coordinate::new(6, 2));
+    }
+
+    #[test]
+    fn test_adjacent() {
+        let coord = Coordinate::new(1, 1);
+        let adj = coord.adjacent();
+
+        assert_eq!(adj[0], Some(Coordinate::new(1, 0))); // up
+        assert_eq!(adj[1], Some(Coordinate::new(2, 1))); // right
+        assert_eq!(adj[2], Some(Coordinate::new(1, 2))); // down
+        assert_eq!(adj[3], Some(Coordinate::new(0, 1))); // left
+    }
+
+    #[test]
+    fn test_adjacent_at_zero() {
+        let coord = Coordinate::new(0, 0);
+        let adj = coord.adjacent();
+
+        assert_eq!(adj[0], None); // up
+        assert_eq!(adj[1], Some(Coordinate::new(1, 0))); // right
+        assert_eq!(adj[2], Some(Coordinate::new(0, 1))); // down
+        assert_eq!(adj[3], None); // left
+    }
+
+    #[test]
+    fn test_surrounding() {
+        let coord = Coordinate::new(1, 1);
+        let surr = coord.surrounding();
+
+        assert_eq!(surr[0], Some(Coordinate::new(1, 0))); // up
+        assert_eq!(surr[1], Some(Coordinate::new(2, 0))); // topright
+        assert_eq!(surr[2], Some(Coordinate::new(2, 1))); // right
+        assert_eq!(surr[3], Some(Coordinate::new(2, 2))); // bottomright
+        assert_eq!(surr[4], Some(Coordinate::new(1, 2))); // down
+        assert_eq!(surr[5], Some(Coordinate::new(0, 2))); // bottomleft
+        assert_eq!(surr[6], Some(Coordinate::new(0, 1))); // left
+        assert_eq!(surr[7], Some(Coordinate::new(0, 0))); // topleft
+    }
+
+    #[test]
+    fn test_ord() {
+        let a = Coordinate::new(1, 2);
+        let b = Coordinate::new(1, 3);
+        let c = Coordinate::new(2, 1);
+
+        assert!(a < b);
+        assert!(b < c);
+    }
 }
